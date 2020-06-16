09ed689a1cdb5c5f0e3d5e5df4eb411681e6b95c
Bond1q <67001901+Bond1q@users.noreply.github.com>
2020-06-16 15:01:50 +0300

    Delete keyboard.py

--- keyboard.py
+++
@@ -1,9 +0,0 @@
-from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton
-button1 = KeyboardButton('А')
-button2 = KeyboardButton('Б')
-button3 = KeyboardButton('В')
-button4 = KeyboardButton('Г')
-button5 = KeyboardButton('Д')
-
-markup1 = ReplyKeyboardMarkup(resize_keyboard=True,one_time_keyboard=True).row(button1,button2,button3,button4,button5)
-markup2 = ReplyKeyboardMarkup(resize_keyboard=True,one_time_keyboard=True).row(button1,button2,button3,button4)
